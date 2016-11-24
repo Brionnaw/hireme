80f4a239ca4fc5d6a05f86d4f25b630e3f2e93c7
Brionna White <Brionnanwhite@gmail.com>
2016-11-23 18:43:00 -0800

    add

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,6 +4,16 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
+// TOGGLE FUNCTION FOR NAV BAR ICON
+function togglevisibility(id) {
+  var e = document.getElementById(id);
+  if(e.style.display == 'none'){
+    e.style.display = 'block';
+  }else{
+    e.style.display = 'none';
+  }
+}
+
 (function($) {
 
 	skel.breakpoints({
